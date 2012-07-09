f453aa2b7e3a3f86c7fde922b6c6e5977d460d43
Jan Reckfort <janreckfort@uni-duesseldorf.de>
2012-07-09 11:10:06 +0200

    Kleine fixes

--- src/Client/WaitBothPlayers.java
+++ src/Client/WaitBothPlayers.java
@@ -14,14 +14,11 @@ public class WaitBothPlayers extends Thread {
 
 	public void run() {
 		boolean wait = true;
-		System.out.println("WaitBothPlayers: Gestartet.");
 		while (wait == true) {
 			if (ClientGameReader.gameOn == true) {
 				wait = false;
 				Game.main(null);
 				Interface.closeMenuOpenGame();
-
-				System.out.println("WaitBothPlayers: Beendet.");
 
 				new RepaintThread().start();
 
